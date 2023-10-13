627168b81b27a6a9fa7b6a9a884a1e8475d9fd13
CarloCassini <c.cassini87@gmail.com>
2023-10-13 16:29:09 +0200

    stampa titoli da Database

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,16 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.layout')
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('main-content')
+<h1 class="debug">
+ciccio
+</h1>
 
-  <title>Laravel</title>
-
-  <!-- Fonts -->
-  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-  <!-- Styles -->
-  @vite('resources/js/app.js')
-</head>
-
-<body>
-  <h1>ciao!</h1>
-</body>
-
-</html>
+@if (@isset($movies))
+@foreach ($movies as $movie)
+{{$movie->title}}
+    <br>
+@endforeach
+    
+@endif
+    
+@endsection
